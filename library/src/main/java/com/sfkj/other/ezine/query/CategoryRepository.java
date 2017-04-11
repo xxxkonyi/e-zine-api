28f4a4cad2b40681c334aa8e8850617be61b5db4
帮主 <xxxkonyi@gmail.com>
2017-04-12 00:07:33 +0800

    基础设施升级，数据库配置修改

--- library/src/main/java/com/sfkj/other/ezine/query/CategoryRepository.java
+++ library/src/main/java/com/sfkj/other/ezine/query/CategoryRepository.java
@@ -6,6 +6,4 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface CategoryRepository extends MongoRepository<Category, String> {
 
-
-
 }
